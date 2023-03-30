7857097b3d1bae54807a9a55ec76ed9bfbec8f34
Gus-96 <augusto.da.silva@live.com>
2023-03-30 16:20:37 -0300

    ajustes finais

--- script.js
+++ script.js
@@ -81,6 +81,7 @@ function to_call_number(){
   const formattedPhoneNumber = formatPhoneNumberBR(phone);
 
   if (phone != "" && phone.length >= 10 && isNumeric(phone)){ 
+    
     if (check_phone(phone)) {
       inputStatusPhone.value = `Chamando ... ${formattedPhoneNumber} `
       calling.currentTime = 0;
@@ -90,7 +91,7 @@ function to_call_number(){
       hung_up.style.display = "block";
     } else{
       alert("Telefone inválido, informe: DDD + Número");
-    }
+    }script.js
 
   } else if (phone == ""){
     alert("Informe um número de telefone!");
@@ -113,14 +114,10 @@ function check_phone(phone) {
     "11", "12", "13", "14", "15", "16", "17", "18", "19",
     "21", "22", "24", "27", "28",
     "31", "32", "33", "34", "35", "37", "38",
-    "41", "42", "43", "44", "45", "46",
-    "47", "48", "49",
+    "41", "42", "43", "44", "45", "46", "47", "48", "49",
     "51", "53", "54", "55",
-    "61", "62", "64", "63", "65", "66",
-    "67", "68",
-    "69",
-    "71", "73", "74", "75", "77",
-    "79",
+    "61", "62", "64", "63", "65", "66", "67", "68", "69",
+    "71", "73", "74", "75", "77", "79",
     "81", "82", "83", "84", "85", "86", "87", "88", "89",
     "91", "92", "93", "94", "95", "96", "97", "98", "99"
   ];
